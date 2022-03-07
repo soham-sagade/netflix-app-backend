a104c8fc603edaf6d37b71968530604237f55378
soham-sagade <sohamsagade33@gmail.com>
2022-03-07 20:52:17 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/usersRouter');
 const movieRouter = require('./routes/moviesRouter');
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 8001;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(movieRouter);
 app.use(listRouter);
